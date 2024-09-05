f3f5d71b8f0c0a06b1758a2b7a9cba6f93145322
Jared-Tyler <Jared.Moodley@mazars.co.za>
2024-09-05 09:00:07 +0200

    Created Reg and Login Portal

--- config.py
+++ config.py
@@ -4,5 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
     SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_TOKEN_LOCATION = ['headers', 'cookies']
+    JWT_COOKIE_SECURE = True  # Ensure this is True in production (requires HTTPS)
+    JWT_ACCESS_COOKIE_NAME = 'access_token'  # Cookie name for the JWT token
+    JWT_COOKIE_CSRF_PROTECT = True  # Protect cookies from CSRF attacks
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = True  # Set to True if using HTTPS
